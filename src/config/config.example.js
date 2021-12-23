ab72d133bbf907c482ceba123e150ca9ade062ba
Matthew Nguyen <itsmatthewnguyen@gmail.com>
2021-12-23 14:24:31 -0800

    Update config.example.js

--- src/config/config.example.js
+++ src/config/config.example.js
@@ -1,5 +1,5 @@
 // Put your API key goes here!
-var API_KEY = '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';
-var YOUTUBE_API_KEY = 'AIzaSyDvJ2GKw6b_kNo-GU-WQOOUQ85qCp66qhE';
+var API_KEY = 'API_KEY';
+var YOUTUBE_API_KEY = 'YOUTUBE_API_KEY';
 
 export { API_KEY, YOUTUBE_API_KEY };
